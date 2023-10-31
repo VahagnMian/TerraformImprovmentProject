d318a7337d7992a9995f5a7763c499fd8353abf2
VahagnScylla <vahagn.martirosyan@scylla.ai>
2023-10-31 16:35:01 +0400

    Feature: processing json to get valid go objects

--- app/main.go
+++ app/main.go
@@ -7,71 +7,55 @@ import (
 	"os/exec"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+type TerraformOutput struct {
+	Sensitive bool          `json:"sensitive"`
+	Type      []interface{} `json:"type"` // Use interface{} as type is a mixed structure.
+	Value     []string      `json:"value"`
 }
+
+type TerraformOutputs map[string]TerraformOutput
 
 func main() {
+	// JSON data
+	//jsonData := `{
+	//    "private_subnet_ids": {
+	//        "sensitive": false,
+	//        "type": ["tuple", ["string", "string", "string"]],
+	//        "value": ["subnet-0e630bed62b4f91a9", "subnet-0168fa5aba8893464", "subnet-0cdedca684b1a779a"]
+	//    },
+	//    "public_subnet_ids": {
+	//        "sensitive": false,
+	//        "type": ["tuple", ["string", "string", "string"]],
+	//        "value": ["subnet-0e630bed62b4f91a9", "subnet-0168fa5aba8893464", "subnet-0cdedca684b1a779a"]
+	//    }
+	//}`
 
-	//// Open the template file
-	//file, err := os.Open("../eks/main.tf")
-	//if err != nil {
-	//	panic(err)
-	//}
-	//defer file.Close()
-	//
-	//// Regular expression to match getValueFrom(...) pattern
-	//regex := regexp.MustCompile(`getValueFrom\(([^)]+)\)`)
-	//
-	//scanner := bufio.NewScanner(file)
-	//for scanner.Scan() {
-	//	line := scanner.Text()
-	//
-	//	// Find all matches in the line
-	//	matches := regex.FindAllStringSubmatch(line, -1)
-	//
-	//	for _, match := range matches {
-	//		// match[0] is the whole match, match[1] is the captured group
-	//		fmt.Println("Match: ", match)
-	//		fmt.Println("Match[0]: ", match[0])
-	//		fmt.Println("Match[1]: ", match[1])
-	//		//replacement := getValueFrom(match[1])
-	//
-	//		// Replace the matched string with its processed value
-	//		//line = strings.Replace(line, match[0], replacement, 1)
-	//	}
-	//
-	//	fmt.Println(line)
-	//}
-	//
-	//if err := scanner.Err(); err != nil {
-	//	panic(err)
-	//}
+	outputData := string(getAllOutputs("../vpc"))
 
-	outputVPC := getAllOutputs("../vpc/")
-
-	e := json.Unmarshal(outputVPC, &c)
-
-	if e != nil {
-		log.Panicf("Error happend:", e)
+	var outputs TerraformOutputs
+	err := json.Unmarshal([]byte(outputData), &outputs)
+	if err != nil {
+		log.Fatalf("Error parsing JSON: %s", err)
 	}
-}
 
-func getValueFrom(input string) string {
-	return "Processed Value: " + input
+	json.MarshalIndent(&outputs, "", "  ")
+
+	fmt.Printf("Parsed Outputs: %+v\n", outputs)
+
 }
 
 func getAllOutputs(modulePath string) []byte {
 	cmd := exec.Command("terraform", "output", "-json")
 	cmd.Dir = modulePath
 
-	// Run the command
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error executing Terraform:", err)
 	}
 
 	return output
+}
+
+func getValueFrom(input string) string {
+	return "Processed Value: " + input
 }
