4ca9b2b6d59935ede298f9174ddc8cc9bde2c963
VahagnMian <vahagn.mian@gmail.com>
2023-11-05 12:40:15 +0400

    Add HCL processing basic functionality

--- app/main.go
+++ app/main.go
@@ -1,51 +1,32 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/hcl"
 	"log"
 	"os/exec"
 )
 
-type TerraformOutput struct {
-	Sensitive bool          `json:"sensitive"`
-	Type      []interface{} `json:"type"` // Use interface{} as type is a mixed structure.
-	Value     []string      `json:"value"`
+func main() {
+
+	outputs := getAllOutputs("../vpc", false)
+
+	fmt.Println(string(outputs))
+
+	getValueByKey("private_subnet_ids", parseHCL(string(outputs)))
+
 }
 
-type TerraformOutputs map[string]TerraformOutput
+func getAllOutputs(modulePath string, json bool) []byte {
 
-func main() {
-	// JSON data
-	//jsonData := `{
-	//    "private_subnet_ids": {
-	//        "sensitive": false,
-	//        "type": ["tuple", ["string", "string", "string"]],
-	//        "value": ["subnet-0e630bed62b4f91a9", "subnet-0168fa5aba8893464", "subnet-0cdedca684b1a779a"]
-	//    },
-	//    "public_subnet_ids": {
-	//        "sensitive": false,
-	//        "type": ["tuple", ["string", "string", "string"]],
-	//        "value": ["subnet-0e630bed62b4f91a9", "subnet-0168fa5aba8893464", "subnet-0cdedca684b1a779a"]
-	//    }
-	//}`
+	var cmd *exec.Cmd
 
-	outputData := string(getAllOutputs("../vpc"))
-
-	var outputs TerraformOutputs
-	err := json.Unmarshal([]byte(outputData), &outputs)
-	if err != nil {
-		log.Fatalf("Error parsing JSON: %s", err)
+	if json {
+		cmd = exec.Command("terraform", "output", "-json")
+	} else {
+		cmd = exec.Command("terraform", "output")
 	}
 
-	json.MarshalIndent(&outputs, "", "  ")
-
-	fmt.Printf("Parsed Outputs: %+v\n", outputs)
-
-}
-
-func getAllOutputs(modulePath string) []byte {
-	cmd := exec.Command("terraform", "output", "-json")
 	cmd.Dir = modulePath
 
 	output, err := cmd.Output()
@@ -58,4 +39,23 @@ func getAllOutputs(modulePath string) []byte {
 
 func getValueFrom(input string) string {
 	return "Processed Value: " + input
+}
+
+func parseHCL(hclData string) map[string]interface{} {
+
+	// Create a variable to hold the parsed data
+	var result map[string]interface{}
+
+	// Parse the HCL data
+	err := hcl.Decode(&result, hclData)
+	if err != nil {
+		log.Fatalf("Failed to decode HCL: %s", err)
+	}
+
+	return result
+
+}
+
+func getValueByKey(key string, result map[string]interface{}) {
+	fmt.Println(result[key])
 }
